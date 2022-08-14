723894a0253d1b514090cc7c0753b3e123b7e958
anilappana <anilappana@DESKTOP-U8O24O6>
2022-08-14 14:16:00 -0700

    swagger config

--- SpringbootApp/src/main/java/com/lovetolearn/SpringbootApp/controller/EmployeeController.java
+++ SpringbootApp/src/main/java/com/lovetolearn/SpringbootApp/controller/EmployeeController.java
@@ -1,8 +1,15 @@
 package com.lovetolearn.SpringbootApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+	
+	@GetMapping
+	public String test() {
+		
+	return "test";	
+	}
 }
--- SpringbootApp/src/main/java/com/lovetolearn/SpringbootApp/entities/Employee.java
+++ SpringbootApp/src/main/java/com/lovetolearn/SpringbootApp/entities/Employee.java
@@ -1,8 +1,27 @@
 package com.lovetolearn.SpringbootApp.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Employee {
+
+	@Id
+	public int id;
+
+	public String name;
+
+	public String city;
 
 }
